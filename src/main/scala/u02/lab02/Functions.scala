309a3d8d3761c13f754ef2c48ae6711710bdf1b1
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-03-07 10:54:20 +0100

    add neg function

--- src/main/scala/u02/lab02/Functions.scala
+++ src/main/scala/u02/lab02/Functions.scala
@@ -3,3 +3,5 @@ package u02.lab02
 object Functions :
 
   def parity(value: Int): String = if ( value % 2 == 0 ) "even" else "odd"
+
+  def neg(predicate: String => Boolean): String => Boolean = !predicate(_)
