edde1ca720f45c2be22e65321ef09ce241505b7c
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-03-07 22:58:50 +0100

    create fibonacci function with tail recursion

--- src/main/scala/u02/lab02/Functions.scala
+++ src/main/scala/u02/lab02/Functions.scala
@@ -27,3 +27,12 @@ object Functions :
     case 0 => 0
     case 1 => 1
     case _ => fibonacci(x - 1) + fibonacci(x - 2)
+
+  def fibonacciWithTailRecursion(x: Int): Int =
+    @annotation.tailrec
+    def _fib(x: Int, a: Int , b: Int): Int = x match
+      case 0 => a
+      case 1 => b
+      case _ => _fib(x-1, b, a+b)
+    _fib(x, 0, 1)
+
