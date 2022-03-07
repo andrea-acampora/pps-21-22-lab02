2b8f6c461bbdbafe0da18e953a59287100d3260a
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-03-07 23:25:08 +0100

    create test for shapes functions

--- src/test/scala/u02/FunctionsTest.scala
+++ src/test/scala/u02/FunctionsTest.scala
@@ -4,22 +4,21 @@ import org.junit.Assert.{assertEquals, assertFalse, assertTrue}
 import org.junit.Test
 import u02.lab02.Functions
 import u02.lab02.Functions.{compose, fibonacci, funWithCurrying, funWithNoCurrying, negPredicate, negWithGenerics, parity, valWithCurrying, valWithNoCurrying}
-
+import u02.lab02.Es7.{Shape, perimeter, area}
 
 class FunctionsTest :
 
   @Test def evenParityTest() =
     val evenValue: Int = 2
-    assertEquals(parity(evenValue), "even")
-
+    assertEquals("even", parity(evenValue))
 
   @Test def oddParityTest() =
     val oddValue: Int = 3
-    assertEquals(parity(oddValue), "odd")
+    assertEquals("odd", parity(oddValue))
 
   @Test def parityWithMatchCaseTest() =
     val evenValue: Int = 2
-    assertEquals(Functions.parityWithMatchCase(evenValue), "even")
+    assertEquals("even", Functions.parityWithMatchCase(evenValue))
 
   @Test def negPredicateTest() =
     val emptyPredicate: String => Boolean = _ == ""
@@ -57,10 +56,28 @@ class FunctionsTest :
     assertFalse(funWithCurrying(3)(2)(1))
 
   @Test def testCompose() =
-    assertEquals(compose(_ - 1, _ * 2)(5), 9)
+    assertEquals(9, compose(_ - 1, _ * 2)(5))
 
   @Test def testFibonacci() =
-    assertEquals(fibonacci(10), 55)
+    assertEquals(55, fibonacci(10))
 
   @Test def testFibonacciWithTailRecursion() =
-    assertEquals(fibonacci(10), 55)
+    assertEquals(55, fibonacci(10))
+
+  @Test def testRectanglePerimeter() =
+    assertEquals(30, perimeter(Shape.Rectangle(10,5)), 0)
+
+  @Test def testRectangleArea() =
+    assertEquals(50, area(Shape.Rectangle(10,5)), 0)
+
+  @Test def testCirclePerimeter() =
+    assertEquals(62.83, perimeter(Shape.Circle(10)), 0.1)
+
+  @Test def testCircleArea() =
+    assertEquals(314.16, area(Shape.Circle(10)), 0.1)
+
+  @Test def testSquarePerimeter() =
+    assertEquals(40, perimeter(Shape.Square(10)), 0)
+
+  @Test def testSquareArea() =
+    assertEquals(100, area(Shape.Square(10)), 0)
