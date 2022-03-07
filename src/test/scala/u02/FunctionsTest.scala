87debf21004d25d3bbab4cc59cc84b856a98f4c4
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-03-07 11:10:13 +0100

    add test for neg functions and parity match case

--- src/test/scala/u02/FunctionsTest.scala
+++ src/test/scala/u02/FunctionsTest.scala
@@ -1,14 +1,32 @@
 package u02
 
-import org.junit.Assert.{assertEquals, assertFalse}
+import org.junit.Assert.{assertEquals, assertFalse, assertTrue}
 import org.junit.Test
+import u02.lab02.Functions
 import u02.lab02.Functions.parity
+import u02.lab02.Functions.negPredicate
+
 
 class FunctionsTest :
 
-  @Test def parityTest() =
+  @Test def evenParityTest() =
+    val evenValue: Int = 2
+    assertEquals(parity(evenValue), "even")
+
+
+  @Test def oddParityTest() =
+    val oddValue: Int = 3
+    assertEquals(parity(oddValue), "odd")
+
+  @Test def parityWithMatchCase() =
     val evenValue: Int = 2
     val oddValue: Int = 3
-    assertEquals(parity(evenValue), "even")
-    assertEquals(parity(oddValue), "odd")
-    assertEquals(parity(evenValue), "even")
+    assertEquals(Functions.parityWithMatchCase(evenValue), "even")
+    assertEquals(Functions.parityWithMatchCase(oddValue), "odd")
+
+  @Test def negPredicateTest() =
+    val emptyPredicate: String => Boolean = _ == ""
+    val notEmptyPredicate = negPredicate(emptyPredicate)
+    assertTrue(notEmptyPredicate("foo"))
+    assertFalse(notEmptyPredicate(""))
+    assertTrue(notEmptyPredicate("foo") && !notEmptyPredicate(""))
