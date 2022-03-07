97ec602bba3b43f59124bb4a7738b07697457042
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-03-07 11:08:23 +0100

    add parity with match case implementation

--- src/main/scala/u02/lab02/Functions.scala
+++ src/main/scala/u02/lab02/Functions.scala
@@ -4,4 +4,9 @@ object Functions :
 
   def parity(value: Int): String = if ( value % 2 == 0 ) "even" else "odd"
 
-  def neg(predicate: String => Boolean): String => Boolean = !predicate(_)
+  def parityWithMatchCase(value: Int) =
+    value % 2 match
+      case 0 => "even"
+      case 1 => "odd"
+
+  def negPredicate(predicate: String => Boolean): String => Boolean = !predicate(_)
