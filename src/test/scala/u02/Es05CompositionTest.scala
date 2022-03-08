9f05efd78f28bd793a5b50d466b917cf0ceb6b17
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-03-08 14:42:06 +0100

    test(composition): add test for generic function

--- src/test/scala/u02/Es05CompositionTest.scala
+++ src/test/scala/u02/Es05CompositionTest.scala
@@ -2,9 +2,12 @@ package u02
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
-import u02.lab02.Es05Composition.compose
+import u02.lab02.Es05Composition.{compose, composeWithGenerics}
 
 class Es05CompositionTest :
 
   @Test def testCompose(): Unit =
     assertEquals(9, compose(_ - 1, _ * 2)(5))
+
+  @Test def testComposeWithGenerics(): Unit =
+    assertEquals(9, composeWithGenerics[Double, Double, Double](_ - 1, _ * 2)(5), 0)
