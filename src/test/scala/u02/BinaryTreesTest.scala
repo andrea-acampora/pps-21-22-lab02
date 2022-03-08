a394cb381b1c49c7068e0dadaacd9757f1bb538d
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-03-08 22:30:43 +0100

    test: create test for BTrees functions

--- src/test/scala/u02/BinaryTreesTest.scala
+++ src/test/scala/u02/BinaryTreesTest.scala
@@ -4,20 +4,23 @@ import org.junit.*
 import org.junit.Assert.*
 import BTrees.Tree.*
 
-class BinaryTreesTest {
+class BinaryTreesTest :
   val tree: BTrees.Tree[Int] = Branch(Branch(Leaf(1), Leaf(2)), Leaf(1))
 
-  @Test def testCount(): Unit = {
+  @Test def testRightCount(): Unit =
     assertEquals(2, count(tree, 1))
-    assertEquals(1, count(tree, 2))
-    assertEquals(0, count(tree, 3))
-  }
-  
-  @Test def testSize(): Unit =
+
+  @Test def testWrongCount(): Unit =
+    assertNotEquals(3, count(tree, 1))
+
+  @Test def testRightSize(): Unit =
     assertEquals(3, size(tree))
 
-  @Test def testFind(): Unit = {
+  @Test def testWrongSize(): Unit =
+    assertNotEquals(2, size(tree))
+
+  @Test def testRightFind(): Unit =
     assertTrue(find(tree, 2))
+
+  @Test def testWrongFind(): Unit =
     assertFalse(find(tree, 4))
-  }
-}
