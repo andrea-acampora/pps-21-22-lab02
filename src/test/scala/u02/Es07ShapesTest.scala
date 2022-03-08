29fd9398e2c8f4fb06a7ae63f713f24ca86ce1ff
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-03-08 22:29:32 +0100

    refactor: remove magic numbers

--- src/test/scala/u02/Es07ShapesTest.scala
+++ src/test/scala/u02/Es07ShapesTest.scala
@@ -7,20 +7,22 @@ import u02.lab02.Es07Shapes.Shape
 
 class Es07ShapesTest :
 
+  val exampleLength: Double = 10
+
   @Test def testRectanglePerimeter(): Unit =
-    assertEquals(30, perimeter(Shape.Rectangle(10,5)), 0)
+    assertEquals(30, perimeter(Shape.Rectangle(exampleLength, exampleLength / 2)), 0)
 
   @Test def testRectangleArea(): Unit =
-    assertEquals(50, area(Shape.Rectangle(10,5)), 0)
+    assertEquals(50, area(Shape.Rectangle(exampleLength, exampleLength / 2)), 0)
 
   @Test def testCirclePerimeter(): Unit =
-    assertEquals(62.83, perimeter(Shape.Circle(10)), 0.1)
+    assertEquals(2 * Math.PI * exampleLength, perimeter(Shape.Circle(exampleLength)), 0.1)
 
   @Test def testCircleArea(): Unit =
-    assertEquals(314.16, area(Shape.Circle(10)), 0.1)
+    assertEquals(exampleLength * exampleLength * Math.PI, area(Shape.Circle(exampleLength)), 0.1)
 
   @Test def testSquarePerimeter(): Unit =
-    assertEquals(40, perimeter(Shape.Square(10)), 0)
+    assertEquals(exampleLength * 4, perimeter(Shape.Square(exampleLength)), 0)
 
   @Test def testSquareArea(): Unit =
-    assertEquals(100, area(Shape.Square(10)), 0)
+    assertEquals(exampleLength * exampleLength, area(Shape.Square(exampleLength)), 0)
