e28f73501ffc050b3bb33deee0290669ee15912d
Andrea Acampora <andrea.acampora@studio.unibo.it>
2022-03-08 22:30:12 +0100

    refactor: create traverse function and adjust previous ones

--- src/main/scala/u02/BTrees.scala
+++ src/main/scala/u02/BTrees.scala
@@ -2,21 +2,18 @@ package u02
 
 object BTrees extends App:
 
-  // A custom and generic binary tree of elements of type A
   enum Tree[A]:
     case Leaf(value: A)
     case Branch(left: Tree[A], right: Tree[A])
 
   object Tree:
-    def size[A](t: Tree[A]): Int = t match
-      case Branch(l, r) => size(l) + size(r)
-      case _ => 1
 
-    def find[A](t: Tree[A], elem: A): Boolean = t match
-      case Branch(l, r) => find(l, elem) || find(r, elem)
-      case Leaf(e) => e == elem
+    def find[A](t: Tree[A], elem: A): Boolean = traverse(t)((l,r) => find(l, elem) || find (r,elem))(_ == elem)
 
-    def count[A](t: Tree[A], elem: A): Int = t match
-      case Branch(l, r) => count(l, elem) + count(r, elem)
-      case Leaf(e) if e == elem => 1
-      case _ => 0
+    def size[A](t: Tree[A]): Int = traverse(t)((l,r)=> size(l) + size(r))(_ => 1)
+
+    def count[A](t: Tree[A], elem: A): Int = traverse(t)((l, r) => count(l, elem) + count(r, elem))(x => if (x == elem) 1 else 0)
+
+    def traverse[A,B](t: Tree[A])(branchFunction: (Tree[A], Tree[A]) => B)(leafFunction:  A => B): B = t match
+      case Branch(left, right) => branchFunction(left, right)
+      case Leaf(value) => leafFunction(value)
